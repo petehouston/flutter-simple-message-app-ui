4990f271fe1cfdffdbc5e5b95ed676742bdcb564
Pete Houston <contact@petehouston.com>
2019-02-02 21:33:10 +0700

    First commit

---
+++ lib/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String avatarUrl;
+  final String name;
+  final String datetime;
+  final String message;
+
+  ChatModel({this.avatarUrl, this.name, this.datetime, this.message});
+
+  static final List<ChatModel> dummyData = [
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './chat_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('fluttermaster.com - Message Chat'),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: ChatModel.dummyData.length,
+          itemBuilder: (context, index) {
+            ChatModel _model = ChatModel.dummyData[index];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(_model.avatarUrl),
+              ),
+              title: Text(_model.name),
+              subtitle: Text(_model.message),
+              trailing: Text(_model.datetime),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
